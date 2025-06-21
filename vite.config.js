cc5ba15e6a3623fba1db39e5a1d6329ab9ff54c7
MichaelOhana <157629142+MichaelOhana@users.noreply.github.com>
2025-06-21 20:10:34 +0300

    moved thank-you Outside full landing

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         rollupOptions: {
+            input: {
+                main: 'index.html',
+                course: 'src/course/index.html'
+            },
             output: {
                 entryFileNames: `assets/[name].[hash].js`,
                 chunkFileNames: `assets/[name].[hash].js`,
